d9ebcc429a5ff30e4f256799139bfbaa7889a027
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-09-08 19:15:28 +0500

    Update xmonad config
    Signed-off-by: Taimoor Zaeem <taimoorzaeem@gmail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,21 @@
+-- xmonad 0.18.0
+
 import XMonad
 import XMonad.Util.EZConfig
+import XMonad.Actions.SpawnOn
+import XMonad.Layout.Grid
+
+import Control.Monad (replicateM_)
+
+myTerminal = "gnome-terminal --hide-menubar"
 
 main :: IO ()
 main = xmonad def
-    { terminal = "gnome-terminal" }
+    { terminal = myTerminal
+    , startupHook = spawnTerminals
+    , layoutHook = Grid ||| layoutHook def -- 2 x 2 Grid
+    }
+
+spawnTerminals :: X ()
+spawnTerminals = do
+    replicateM_ 4 $ spawnOn "1" myTerminal -- Spawn 4 terminals in workspace 1
